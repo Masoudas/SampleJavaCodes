0f64b707e379753a248bec758db45e6d031df7bb
Masoudas <Masoudas@hotmai.com>
2020-06-06 12:59:38 +0200

    More enums

--- SampleJavaCodes/src/main/java/EffectiveJava/CreatingAndDestroyingObjects/7_EliminateObsoleteObjectReferences.java
+++ SampleJavaCodes/src/main/java/EffectiveJava/CreatingAndDestroyingObjects/7_EliminateObsoleteObjectReferences.java
@@ -1,7 +1,11 @@
 package EffectiveJava.CreatingAndDestroyingObjects;
 
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import java.util.Arrays;
 import java.util.EmptyStackException;
+import java.util.HashMap;
+import java.util.WeakHashMap;
 
 /**
  * As is evident, the problem with the following implementation of stack is that
@@ -9,9 +13,40 @@ import java.util.EmptyStackException;
  * size), then it will still remain inside the array (and in other words inside
  * memory). Hence, we tend to get outOfMemory. To avoid this we null the
  * reference by writing Object element = elements[--size]; and then
- * elements[size] = null; 
+ * elements[size] = null;
  * 
- * ////////////////////////// DIDN'T FINISH THIS ///////////////////////////////
+ * Note that nulling objects should be the exception rather than the norm.
+ * 
+ * When should we null objects? When an object manages its own memory, like the
+ * example we saw here. Also caching objects is an important reason why this
+ * happens.
+ * 
+ * When caching, WeakHashMaps can be used. If you’re lucky enough to implement a
+ * cache for which an entry is relevant exactly so long as there are references
+ * to its key outside of the cache, represent the cache as a WeakHashMap;
+ * entries will be removed automatically after they become obsolete (See example
+ * below).
+ * 
+ * More commonly, the useful lifetime of a cache entry is less well defined,
+ * with entries becoming less valuable over time. Under these circumstances, the
+ * cache should occasionally be cleansed of entries that have fallen into
+ * disuse. This can be done by a background thread (perhaps a
+ * ScheduledThreadPoolExecutor ) or as a side effect of adding new entries to
+ * the cache. The LinkedHashMap class facilitates the latter approach with its
+ * removeEldestEntry method. For more sophisticated caches, you may need to use
+ * java.lang.ref directly.
+ * 
+ * A third common source of memory leaks is listeners and other callbacks. If
+ * you implement an API where clients register callbacks but don’t deregister
+ * them explicitly, they will accumulate unless you take some action. One way to
+ * ensure that callbacks are garbage collected promptly is to store only weak
+ * references to them, for instance, by storing them only as keys in a
+ * WeakHashMap . Because memory leaks typically do not manifest themselves as
+ * obvious failures, they may remain present in a system for years. They are
+ * typically discovered only as a result of careful code inspection or with the
+ * aid of a debugging tool known as a heap profiler. Therefore, it is very
+ * desirable to learn to anticipate problems like this before they occur and
+ * prevent them from happening.
  */
 
 class Stack {
@@ -41,5 +76,66 @@ class Stack {
     private void ensureCapacity() {
         if (elements.length == size)
             elements = Arrays.copyOf(elements, 2 * size + 1);
+    }
+
+}
+
+class WeakHashMapUse {
+    private static void strongSoftAndWeakReference() {
+        // The variable prime has a strong reference to object 1.
+        // Any object with strong reference is not garbage collected.
+        Integer prime = 1;
+
+        // Any object with soft reference will remain in memory, until the JVM
+        // absolutely needs memory,
+        // then it is GCed
+        Integer prime1 = 1;
+        SoftReference<Integer> softReference = new SoftReference<Integer>(prime1);
+        prime1 = null;
+
+        System.out.println(softReference.get()); // This reference is valid, but if JVM absolutely needs memory, will
+                                                 // perish
+
+        // Any object with weak reference will remain in memory, but JVM will
+        // be eager to remove it and use its memory instead.
+        Integer prime2 = 1;
+        WeakReference<Integer> weakReference = new WeakReference<Integer>(prime2);
+        prime2 = null;
+
+        System.out.println(weakReference.get()); // This reference becomes invalid as soon as JVM needs memory, which
+        // is not right now!
+
+    }
+
+    public static void main(String[] args) {
+        strongSoftAndWeakReference();
+        whatIsWeakHashMap();
+
+    }
+
+    private static void whatIsWeakHashMap() {
+        String key1 = "A";
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put(key1, "A");
+
+        WeakHashMap<String, String> weakHashMap = new WeakHashMap<>();
+        weakHashMap.put(key1, "A");
+
+        System.out.println(hashMap.size());
+        System.out.println(weakHashMap.size());
+
+        // Now we null the key
+        key1 = null;
+
+        // We see that all of a sudden, the strong hash map keeps everything, whereas
+        // the weak has removed everything, or removes every key as soon as JVM needs
+        // memory.
+        // The point is that if the dictionary is the only remaining reference, we don't
+        // remove it
+        // with the strong hashmap, whereas we remove it with the weak one.
+        System.out.println(hashMap.size());
+        System.out.println(weakHashMap.size());
+
     }
 }
---
+++ SampleJavaCodes/src/main/java/EffectiveJava/EnumsAndAnnotations/4_UseEnumMapInsteadOfOrdinalIndexing.java
@@ -0,0 +1,129 @@
+package EffectiveJava.EnumsAndAnnotations;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * We wanting to associate data with an enum, create a hash table, with keys
+ * being the enums and values being the stuff that you want to associate with
+ * them. Don't use the ordinals of the enum (Duh!).
+ * 
+ * Instead, use an EnumMap, which associates objects with an enumeration.
+ * Especially, notice how clean the printing is! The difference between the
+ * EnumMap version and the stream version is that with the EnumMap, we get an
+ * empty set for every key, whereas with Stream we get keys only for ones with
+ * an actual objects.
+ * 
+ * The next example is map between two enums. The ordinal version (PhaseI) is of
+ * course is not a good idea. We should rather create an EnumMap from keys of
+ * one set, and then assign our values to it. It's very interesting that if we
+ * add new values (new transitions), the remainder of the code remains
+ * unchanged.
+ */
+
+class Plant {
+    enum LifeCycle {
+        ANNUAL, PERENNIAL, BIENNIAL
+    }
+
+    final String name;
+    final LifeCycle lifeCycle;
+
+    Plant(String name, LifeCycle lifeCycle) {
+        this.name = name;
+        this.lifeCycle = lifeCycle;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    private void associateToLiftCycleUsingOrdinalWRONG() {
+        // Using ordinal() to index into an array - DON'T DO THIS!
+        Set<Plant>[] plantsByLifeCycle = (Set<Plant>[]) new Set[Plant.LifeCycle.values().length];
+        for (int i = 0; i < plantsByLifeCycle.length; i++)
+            plantsByLifeCycle[i] = new HashSet<>();
+
+        Plant[] garden = { new Plant("a", LifeCycle.ANNUAL), new Plant("b", LifeCycle.ANNUAL) };
+        for (Plant p : garden)
+            plantsByLifeCycle[p.lifeCycle.ordinal()].add(p);
+
+        // Print the results
+        for (int i = 0; i < plantsByLifeCycle.length; i++) {
+            System.out.printf("%s: %s%n", Plant.LifeCycle.values()[i], plantsByLifeCycle[i]);
+        }
+
+    }
+
+    private void associateToLiftCycleUsingOrdinalRIGHT() {
+        Map<Plant.LifeCycle, Set<Plant>> plantsByLifeCycle = new EnumMap<>(Plant.LifeCycle.class);
+        for (Plant.LifeCycle lc : Plant.LifeCycle.values())
+            plantsByLifeCycle.put(lc, new HashSet<>());
+
+        Plant[] garden = { new Plant("a", LifeCycle.ANNUAL), new Plant("b", LifeCycle.ANNUAL) };
+        for (Plant p : garden)
+            plantsByLifeCycle.get(p.lifeCycle).add(p);
+
+        System.out.println(plantsByLifeCycle);
+    }
+
+    private void associateToLiftCycleUsingOrdinalWithStreamRIGHT() {
+        // Using a stream and an EnumMap to associate data with an enum
+        Plant[] garden = { new Plant("a", LifeCycle.ANNUAL), new Plant("b", LifeCycle.ANNUAL) };
+        System.out.println(Arrays.stream(garden).collect(
+                Collectors.groupingBy(p -> p.lifeCycle, () -> new EnumMap<>(LifeCycle.class), Collectors.toSet())));
+    }
+
+    public static void main(String[] args) {
+        new Plant("a", LifeCycle.ANNUAL).associateToLiftCycleUsingOrdinalRIGHT();
+    }
+}
+
+enum PhaseI {
+    SOLID, LIQUID, GAS;
+
+    public enum Transition {
+        MELT, FREEZE, BOIL, CONDENSE, SUBLIME, DEPOSIT;
+
+        // Rows indexed by from-ordinal, cols by to-ordinal
+        private static final Transition[][] TRANSITIONS = { { null, MELT, SUBLIME }, { FREEZE, null, BOIL },
+                { DEPOSIT, CONDENSE, null } };
+
+        // Returns the phase transition from one phase to another
+        public static Transition from(PhaseI from, PhaseI to) {
+            return TRANSITIONS[from.ordinal()][to.ordinal()];
+        }
+    }
+}
+
+enum Phase {
+    SOLID, LIQUID, GAS;
+
+    public enum Transition {
+        MELT(SOLID, LIQUID), FREEZE(LIQUID, SOLID), BOIL(LIQUID, GAS), CONDENSE(GAS, LIQUID), SUBLIME(SOLID, GAS),
+        DEPOSIT(GAS, SOLID);
+
+        private final Phase from;
+        private final Phase to;
+
+        Transition(Phase from, Phase to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        // Initialize the phase transition map (this is very easy indeed).
+        private static final Map<Phase, Map<Phase, Transition>> m = Stream.of(values())
+                .collect(Collectors.groupingBy(t -> t.from, () -> new EnumMap<>(Phase.class),
+                        Collectors.toMap(t -> t.to, t -> t, (x, y) -> y, () -> new EnumMap<>(Phase.class))));
+
+        public static Transition from(Phase from, Phase to) {
+            return m.get(from).get(to);
+        }
+    }
+}
