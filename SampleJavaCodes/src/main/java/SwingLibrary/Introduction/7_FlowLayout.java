ae6ab9f941bcd3df9f6da2e1947f172620e67e51
Masoudas <Masoudas@hotmai.com>
2020-06-08 07:38:14 +0200

    Border layout

--- SampleJavaCodes/src/main/java/SwingLibrary/Introduction/7_FlowLayout.java
+++ SampleJavaCodes/src/main/java/SwingLibrary/Introduction/7_FlowLayout.java
@@ -6,6 +6,7 @@ import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * It sets the components horizontally (left to right or right to left), then
@@ -19,13 +20,26 @@ import javax.swing.JFrame;
  * An example is given below. The pack method is used, so that we don't have to
  * specify bounds for every component.
  * 
- * By default, all components are aligned to the center of the constructor. You
- * can change this by defining the alignment of the FlowLayout in its
- * constructor. We LEFT, RIGHT, CENTER, LEADING, TRAILING. For the last two, if
- * the orientation is left to right, alignment is done with the left most component,
+ * By default, all components are aligned to the center of the frame. You can
+ * change this by defining the alignment of the FlowLayout in its constructor.
+ * We LEFT, RIGHT, CENTER, LEADING, TRAILING. For the last two, if the
+ * orientation is left to right, alignment is done with the left most component,
  * and vice versa.
  * 
- * You can also set the gap between components in the constructor (or using setHGap and setVGap).
+ * You can also set the gap between components in the constructor (or using
+ * setHGap and setVGap).
+ * 
+ * Note also that flowLayout tries to put all components into one row. Hence, it
+ * does not ask for a row size (I think it just looks for the highest row).
+ * Moreover, when the number of components exceed a row, it does not put them in
+ * the next row, it rather hides them! Try and play with the size of the frame
+ * size when displayed to see this effect.
+ * 
+ * As another example, we use a JPanel (which has FlowLayout as manager) and try
+ * and add it to another JFrame. The point is that again, we'd only have one
+ * row containing both the buttons and the JFrame. Moreover, we'll never see the JPanel
+ * break down into two rows (try and play with frame size after compilation). Why?
+ * Because ... I've no idea!
  */
 
 class OrientationOfComponents {
@@ -62,7 +76,7 @@ class UsingFlowLayout {
     }
 }
 
-class UsingFlowLayoutWithGapAndAlignemnt{
+class UsingFlowLayoutWithGapAndAlignemnt {
     public static void main(String[] args) {
         int hGap = 10;
         int vGap = 30;
@@ -71,10 +85,9 @@ class UsingFlowLayoutWithGapAndAlignemnt{
         JFrame frame = new JFrame("Flow Layout Test");
         frame.applyComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         Container contentPane = frame.getContentPane();
         contentPane.setLayout(flowLayout);
-
 
         for (int i = 1; i <= 3; i++) {
             contentPane.add(new JButton("Button " + i));
@@ -82,5 +95,31 @@ class UsingFlowLayoutWithGapAndAlignemnt{
 
         frame.pack();
         frame.setVisible(true);
+    }
+}
+
+class JPanelInsideJFrame{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("With Pannel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contentPane = frame.getContentPane();
+        contentPane.setLayout(new FlowLayout(FlowLayout.LEADING));
+        contentPane.applyComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        
+        for (int i = 1; i <= 3; i++) {
+            contentPane.add(new JButton("Button " + i));
+        }
+
+        JPanel panel = new JPanel();
+        for (int i = 0; i < 3; i++) {
+            panel.add(new JButton("Button " + i));            
+        }
+
+        contentPane.add(panel);
+        
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }
