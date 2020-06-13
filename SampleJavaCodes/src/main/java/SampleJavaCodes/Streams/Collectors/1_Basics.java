17e0a0349dfe0273f35644bfd59952ab255e336b
Masoudas <Masoudas@hotmai.com>
2020-06-13 11:29:20 +0200

    Add collectors

--- SampleJavaCodes/src/main/java/SampleJavaCodes/Streams/Collectors/1_Basics.java
+++ SampleJavaCodes/src/main/java/SampleJavaCodes/Streams/Collectors/1_Basics.java
@@ -1,6 +1,13 @@
 package SampleJavaCodes.Streams.Collectors;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * What are collectors? It is possible (indeed, common) to obtain a stream from
@@ -17,13 +24,56 @@ import java.util.stream.Collector;
  * most famous two implementations are Collectors.toSet and Collectors.toList.
  * 
  * The second form of collect is <> R collect(Supplier<R> target, BiConsumer<R,
- * ? super T> accumulator, BiConsumer<R, R> combiner). To understand the interfaces
- * Supplier and BiConsumer, refer to functional interfaces package first.
+ * ? super T> accumulator, BiConsumer<R, R> combiner). To understand the
+ * interfaces Supplier and BiConsumer, refer to functional interfaces package
+ * first.
  * 
+ * Ok, so now, the accumulator is the method responsible for adding objects to
+ * this target, whose Type is indicated by R. Note that for biConsumer we have
+ * void accept(C obj, D obj2), hence we accept obj2 (of type T) to put it the in
+ * target obj (of type R).
+ * 
+ * The combiner is responsible for adding different objects of Type R here. What
+ * is that used for? If each stream generates a new object of type R, we need to
+ * append them together.
+ * 
+ * In collecter example, I try to put an stream of ints into a linked list.
  */
 class Basics {
     public static void main(String[] args) {
         Collector<?, ?, ?> collector;
+    }
+
+}
+
+class CollectToLinkedList {
+    public static void main(String[] args) {
+        // A special note on the add all. If it has duplicate elements, then it will
+        // (same class reference), it will be eliminated!
+        LinkedList<Integer> listOfInt = IntStream.range(1, 10).collect(() -> {
+            return new LinkedList<>();
+        }, (list, obj) -> list.add(obj), (list1, list2) -> {
+            list1.addAll(list2);
+        });
+
+        // This is a more sophistaced version of the previous verbose thing
+        LinkedList<Integer> list1OfInt = IntStream.range(1, 10).collect(LinkedList::new, LinkedList::add,
+                LinkedList::addAll);
+
+        // To add two lists together so that the duplicates are remaining, we need to
+        // use the flatmap of the stream
+        LinkedList<Integer> list2OfIntEnsuringDuplicates = IntStream.range(1, 10).collect(LinkedList::new,
+                LinkedList::add, (listOne, listTwo) -> {
+                    Stream.of(listOne, listTwo).flatMap(x -> x.stream()).collect(Collectors.toList());
+                });
+
+        // Standard method that returns a list interface
+        // List<Integer> list1OfInt = IntStream.range(1,
+        // 10).collect(Collectors.toList());
+
+        System.out.println(listOfInt);
+        System.out.println(list1OfInt);
+
     }
 
 }
--- SampleJavaCodes/src/main/java/SampleJavaCodes/UtilLibrary/2_ArrayList.java
+++ SampleJavaCodes/src/main/java/SampleJavaCodes/UtilLibrary/2_ArrayList.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class ArrayListExample {
     public static void main(String[] args) {
@@ -58,4 +60,21 @@ class ArrayListExample {
         long[] longArry = Arrays.stream(arr2).mapToLong((t)-> t).toArray();
 
     }
+
+    public void mergingTwoListKeepingTheDuplicates() {
+        ArrayList<String> listOne = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e"));
+
+        ArrayList<String> listTwo = new ArrayList<>(Arrays.asList("a", "b", "c", "f", "g"));
+
+        List<String> combinedList = Stream.of(listOne, listTwo).flatMap(x -> x.stream()).collect(Collectors.toList());
+    }
+
+    public void mergingTwoListNotKeepingTheDuplicates() {
+        ArrayList<String> listOne = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e"));
+
+        ArrayList<String> listTwo = new ArrayList<>(Arrays.asList("a", "b", "c", "f", "g"));
+
+        listOne.addAll(listTwo);
+    }
+
 }
