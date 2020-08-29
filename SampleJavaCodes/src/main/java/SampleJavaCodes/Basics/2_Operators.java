502acb6e22f8c0712c9dee56ecbb053104ddf085
Masoudas <Masoudas@hotmail.com>
2020-08-29 14:21:54 +0200

    push

---
+++ SampleJavaCodes/src/main/java/SampleJavaCodes/Basics/2_Operators.java
@@ -0,0 +1,6 @@
+/**
+ * 
+ * The normal right shift operator replicates the sign bit.
+ * The >>> is unsigned shift. It shifts, not filling the shift bits with one.
+ * For example 0x80>>>1 would be 0x40.
+ */
